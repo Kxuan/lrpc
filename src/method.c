dc4cb214d75d8510f304519ad3aa4c5861636d99
Zhai Zhaoxuan <zhaizhaoxuan@xiaomi.com>
2017-12-26 15:19:06 +0800

    lrpc: add license header
    Signed-off-by: Zhai Zhaoxuan <zhaizhaoxuan@xiaomi.com>

--- src/method.c
+++ src/method.c
@@ -1,5 +1,18 @@
-/*
- */
+/* Copyright (C) 2017 kXuan <kxuanobj@gmail.com>
+
+   This file is part of the lrpc.
+   The lrpc is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+   The lrpc is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+   You should have received a copy of the GNU Lesser General Public
+   License along with the lrpc; if not, see
+   <http://www.gnu.org/licenses/>.  */
+
 
 #include <utlist.h>
 #include <string.h>
@@ -52,6 +65,11 @@ struct lrpc_method *lrpc_method_create(const char *name, lrpc_method_cb callback
 	}
 	lrpc_method_init(method, name, callback, user_data);
 	return method;
+}
+
+void lrpc_method_destroy(struct lrpc_method *method)
+{
+	free(method);
 }
 
 void method_table_init(struct method_table *table)
